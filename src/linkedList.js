a7252f7068b457e5ad8fbb612b2b7ff01bbd1582
Abraham Fergie <jhnfgie1989@gmail.com>
2017-02-01 16:56:44 -0800

    WIP

--- src/linkedList.js
+++ src/linkedList.js
@@ -55,14 +55,51 @@ export default class linkedList {
     return -1
   }
 
-  insertFirst(data) {
+
+  insertBefore(before, data) {
     let node = new Node(data)
+    let currentNode = this.head
+    while(currentNode.next.data !== before) {
+      currentNode = currentNode.next
+    }
     ++this._length
-    node.next = this.head
-    this.head = node
+    node.next = currentNode.next
+    return currentNode.next = node
   }
 
+  insertAfter(after, data) {
+    let node = new Node(data)
+    let currentNode = this.head
+    while(currentNode.data !== after) {
+      currentNode = currentNode.next
+    }
+    ++this._length
+    node.next = currentNode.next
+    return currentNode.next = node
+  }
 
+  remove(data) {
+    let currentNode = this.head
+    while(currentNode.next.data === data) {
+      currentNode = currentNode.next
+    }
+    --this._length
+    return currentNode.next = this.tail = null
+    // delete this.tail
+  }
 
+  removesFirst() {
+    let currentNode = this.head
+    this.head = currentNode.next
+    --this._length
+  }
 
+  isEmpty() {
+    return this._length === 0 ? true : false
+  }
+
+  clears() {
+    this.head = this.tail =  null
+    this._length = 0
+  }
 }
