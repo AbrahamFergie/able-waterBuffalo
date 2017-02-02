222dbeb9fe46d61665d5d2e876601ce7852c574c
Abraham Fergie <jhnfgie1989@gmail.com>
2017-02-02 08:34:48 -0800

    WIP

--- spec/hashTable.js
+++ spec/hashTable.js
@@ -4,17 +4,45 @@ import HashTable from '../src/hashTable'
 
 chai.use(chaiChange)
 
-describe('hashTable', () => {
+describe.only('hashTable', () => {
   it('exists', () => {
     expect(HashTable).to.be.a('function')
   })
   context('put()', () => {
-    it.only('adds a key-value pair to the hash table.', () => {
+    it('adds a key-value pair to the hash table.', () => {
       const ht = new HashTable()
-      ht.put('name', 'sal')
-      console.log('where are you', ht);
-      expect(ht.dataStore.name).to.eql('sal')
+      ht.put('name', 'frankenstein')
+      expect(ht.size()).to.equal(1)
+      ht.put('19.0', 'stein')
+      expect(ht.size()).to.equal(2)
+      ht.get('19.0')
+      expect(ht.get('19.0')).to.eql('stein')
     })
   })
-
+  context('contains()', () => {
+    it('returns true if the hash table contains the key.', () => {
+      const ht = new HashTable()
+      expect(ht.contains('name')).to.be.false
+      ht.put('name', 'frankenstein')
+      ht.put('19.0', 'stein')
+      expect(ht.contains('name')).to.be.true
+    })
+  })
+  context('remove()', () => {
+    it('removes a key-value pair by key.', () => {
+      const ht = new HashTable()
+      ht.remove('')
+    })
+  })
+  context('iterate()', () => {
+    it('takes a callback function and passes it each key and value in sequence.', () => {
+      const ht = new HashTable()
+      ht.put('name', 'frankenstein')
+      ht.put('19.0', 'stein')
+      ht.put('wsrd', 'the weekend')
+      let arr = ht.iterate('19.0')
+      expect(arr).to.eql([{ key: 'name', value: 'frankenstein' },
+      { key: '19.0', value: 'stein' },{ key: 'wsrd', value: 'the weekend' }])
+    })
+  })
 })
--- src/hashTable.js
+++ src/hashTable.js
@@ -1,37 +1,101 @@
-class Hash {
-  constructor(pair1, pair2) {
-    this.pair = {pair1, pair2},
+class Node {
+  constructor(key, value) {
+    this.key = key,
+    this.value = value,
     this.next = null
   }
 }
 
 export default class HashTable {
   constructor() {
-    this.size = 0
-    this.dataStore = {}
-  }
- 
-  put(name, value) {
-    let hash = new Hash(name)
-
-    this.dataStore = new Hash(name, value)
-    this.next = null
-    ++this.size
+    this._length = 0,
+    this.dataStore = []
   }
 
-  // HashTable.get() {
-  //
-  // }
-  // HashTable.contains() {
-  //
-  // }
-  // HashTable.iterate() {
-  //
-  // }
-  // HashTable.remove() {
-  //
-  // }
-  // HashTable.size() {
-  //
-  // }
+  chain(hash, key, value) {
+    let newHash = new Node(key, value)
+    let currentNode = this.dataStore[hash]
+    while(currentNode.next){
+      if (currentNode.key === key) {
+        throw new Error('data already exists')
+      }else{
+        currentNode = currentNode.next
+      }
+    }
+    currentNode.next = newHash
+  }
+
+  hashFunction(key) {
+    let sum = 0
+    for (let i = 0; i < key.length; i++) {
+      sum += key.charCodeAt(i)
+    }
+    return sum % 31
+  }
+
+  save(hash, key, value) {
+    let newHash = new Node(key, value)
+    this.dataStore[hash] = newHash
+  }
+
+  put(key, value) {
+    let hashed = this.hashFunction(key)
+    ++this._length
+    if(!this.dataStore[hashed]){
+      this.save(hashed, key, value)
+    }else{
+      this.chain(hashed, key, value)
+    }
+  }
+  size() { return this._length }
+
+  get(key) {
+    let hashed = this.hashFunction(key)
+    let pair, i
+    if(this.dataStore[hashed]){
+      for(i in this.dataStore[hashed]){
+        pair = this.dataStore[hashed][i]
+        if(pair.key === key)
+          return pair.value
+      }
+    }
+  }
+
+  contains(key) {
+    var hash = this.hashFunction(key)
+    return this.dataStore[hash] ? true : false
+  }
+
+  pushy(arr, key, value) {
+    let ob = {key, value}
+    arr.push(ob)
+    return arr
+  }
+
+  iterate(key) {
+    let arr = []
+    let hashed = this.hashFunction(key)
+    let currentNode = this.dataStore[hashed]
+    while (currentNode.next) {
+      this.pushy(arr, currentNode.key, currentNode.value)
+      currentNode = currentNode.next
+    }
+    this.pushy(arr, currentNode.key, currentNode.value)
+    return arr
+    console.log(arr);
+  }
+
+  remove(key) {
+    let hashed = this.hashFunction(key)
+    if (this.dataStore[hashed]) {
+      let currentNode = this.dataStore[hashed]
+      while (currentNode.next){
+        if (currentNode.key === key) {
+          delete currentNode.key
+        }else {
+          currentNode = currentNode.next
+        }
+      }
+    }
+  }
 }
